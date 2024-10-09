0b9802f7234205c80a4cfa58d0ba61eb757956bb
Ahmed Nauman Tariq <simplyant@hotmail.com>
2024-10-09 14:41:42 +0100

    adding log project

---
+++ TyreCompare.Log/Helper.cs
@@ -0,0 +1,31 @@
+﻿//using Microsoft.Extensions.Configuration;
+
+namespace TyreCompare.Log;
+
+public class Helper
+{
+    public static string GetLogFilePath(string logFolderPath)
+    {
+        var logFileName = GetLogFileName();
+        var logFilePath = Path.Combine(logFolderPath, logFileName);
+        return logFilePath;
+    }
+
+    public static string GetLogFileName()
+    {
+        string logFileName = @$"{DateTime.Today.Year}{DateTime.Today.Month}{DateTime.Today.Day}.txt";
+        return logFileName;
+    }
+
+    public static string FormatMessageForLogFile(string message, string messageDetail = "")
+    {
+        var newMessage = $"\n=== [INFORMATION]: {DateTime.UtcNow} ===\n{message}\n{messageDetail}\n";
+        return newMessage;
+    }
+
+    public static string FormatExceptionForLogFile(Exception ex, string exceptionHeader)
+    {
+        var newMessage = $"\n=== [EXCEPTION]: {DateTime.UtcNow} ===\n{exceptionHeader}{ex.Message}\n{ex.StackTrace}\n";
+        return newMessage;
+    }
+}
---
+++ TyreCompare.Log/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿namespace TyreCompare.Log;
+
+public interface ILog
+{
+    Task<bool> Log(string message, string messageDetail = "");
+    Task<bool> Log(Exception exception, string exceptionHeader = "");
+}
+
+public interface IDBLogger : ILog
+{ }
+
+public interface IFileLogger : ILog
+{
+    string GetCurrentLogPath();
+}
+
+public interface ILoggingService
+{
+    Task<bool> Log(string message, string messageDetail = "");
+    Task<bool> Log(Exception exception, string exceptionHeader = "");
+}
